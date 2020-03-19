413146f66fabc2a704ee8e173528bbeabb3e9e91
Kevin F <accounts@kevinflo.com>
2020-03-19 15:39:46 -0500

    #9 button to add more items now works

--- front/vendor/js/supplier.js
+++ front/vendor/js/supplier.js
@@ -1,3 +1,14 @@
 $(function () {
-    console.log('hello world2')
+    console.log('supplier js loaded')
+
+    $("#btnAddItem").click(function(event){
+        event.preventDefault()
+        var newCard = $('.card-item').last().clone()
+
+        $('.card-item').last().after(newCard)
+
+        $(".card-item").last().find('input').val('')
+        $("#target").val($("#target option:first").val());
+        $('.card-item').last().find('h3').text("Item " + $(".card-item").length)
+    })
 });
